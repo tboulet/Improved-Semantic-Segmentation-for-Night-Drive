514864f25fba6d78611f761cdcc69782ad714553
Raffaelbdl <raffaelbdl@gmail.com>
2022-04-07 11:37:13 +0200

    :zap: Add pipeline.__init__

---
+++ improved_nightdrive/pipeline/__init__.py
@@ -0,0 +1,5 @@
+
+from improved_nightdrive.pipeline.callback import WandbCallback
+from improved_nightdrive.pipeline.metric import MeanIOU
+from improved_nightdrive.pipeline.pipeline import Evaluation, Training 
+from improved_nightdrive.pipeline.preprocess import AddNoise, Normalize, RandomCrop
