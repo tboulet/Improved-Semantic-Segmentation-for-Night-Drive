4fd2c8c46b7c9f92f643c236e21807ce298a5a15
Raffaelbdl <raffaelbdl@gmail.com>
2022-04-07 11:14:15 +0200

    :art: Improve pipeline

--- improved_nightdrive/pipeline/pipeline.py
+++ improved_nightdrive/pipeline/pipeline.py
@@ -1,167 +1,260 @@
+
 import os
-from typing import List, Optional, TYPE_CHECKING
 import random
-from tqdm import tqdm
-import matplotlib.pyplot as plt
+from typing import List, Optional, Tuple, TYPE_CHECKING
+from warnings import warn
 
 import numpy as np
 from PIL import Image
-
 import tensorflow as tf
-from tensorflow.keras.models import Model
-
-from improved_nightdrive.pipeline.preprocess import Crop
-from improved_nightdrive.pipeline.metrics import MeanIOU
+from tensorflow.keras.models import Model, save_model 
+from tensorflow.keras.losses import CategoricalCrossentropy, MeanSquaredError
+from tqdm import tqdm
 
 if TYPE_CHECKING:
-    from improved_nightdrive.pipeline import Metric, Preprocess
+    from improved_nightdrive.pipeline.callback import Callback
+    from improved_nightdrive.pipeline.metrics import Metric
+    from improved_nightdrive.pipeline.preprocess import Preprocess
 
 
 class Evaluation():
     """Evaluation pipeline of a trained model
 
     Attributes:
-        model: A trained keras model
-        metrics: A list of Metric instances
-        preprocesses: A list of preprocesses to apply IN ORDER
-    """
+        model (Model): A Keras model for segmentation
+        num_classes (int): Number of classes
+        loss (str): A loss function used for training
+            -> available losses : ['cce', 'mse']
 
+        x_dir_path (str): Path to image dataset
+        y_dir_path (str): Path to label dataset
+
+        metrics (List[Metric]): A list of metrics to follow progress
+        preprocesses (List[Preprocess]): A list of preprocesses to apply IN ORDER
+    """
     def __init__(
         self, 
         model: Model,
-        metrics: List['Metric'],
-        preprocesses: Optional[List['Preprocess']] = None
+        num_classes: int,
+        loss: str = 'cce',
+        x_dir_path: str = "./dataset/rgb/",
+        y_dir_path: str = "./dataset/seg/",
+        metrics: List['Metric'] = [],
+        preprocesses: Optional[List['Preprocess']] = [],
      ) -> None:
 
+        assert len(metrics) > 0, "Metrics must be specified"
+        for metric in metrics:
+            assert isinstance(metric, Metric), "Metrics must be of type Metric"
+
         self.model = model
+
         self.metrics = metrics
         self.preprocesses = preprocesses
 
-    def evaluate(self, x: tf.Tensor, y: tf.Tensor) -> List[float]:
-        """Evaluates the model
+        self.num_classes = num_classes
+
+        if loss == 'cce':
+            self.loss = CategoricalCrossentropy()
+        elif loss == 'mse':
+            self.loss = MeanSquaredError()
+        else:
+            warn(loss + " is not implemented, cce used by default")
+            self.loss = CategoricalCrossentropy()
+
+        self.x_dir_path = x_dir_path
+        self.y_dir_path = y_dir_path
+    
+
+    def sample_evaluate(
+        self, 
+        x: tf.Tensor, 
+        y: tf.Tensor
+    ) -> List[float]:
+        """Evaluates the model one one sample
 
         Args:
-            x: Inputs
-            y: Labels
+            x_input (tf.Tensor): Input
+            y_input (tf.Tensor): Label
 
         Returns:
-            _metrics: A list of evaluated metrics
+            metrics (List[float]): A list of evaluated metrics
         """
-        _x = x
-        if self.preprocesses is not None:
-            for p in self.preprocesses:
-                _x = p(_x)
+        if len(x.shape) == 3:
+            x = tf.expand_dims(x, axis=0)
+            y = tf.expand_dims(y, axis=0)
 
-        pred = self.model(_x)
+        for preprocess in self.preprocesses:
+            x, y = preprocess.func(x, y)
+        
+        ypred = self.model(x)
 
-        _metrics = []
-        for m in self.metrics:
-            _m = m(y, pred)
-            _metrics.append(_m)
+        evaluated_metrics = []
+        for metric in self.metrics:
+            evaluated_metrics.append(metric.func(y, ypred))
 
-        return _metrics
+        return evaluated_metrics
 
 
 class Training():
     """Training pipeline of a model
 
     Attributes:
-        model: A keras model
-        loss: A loss function that will be used for training
-            available losses : ['cce', 'mse']
-        metrics: A list of Metric to follow progress
-        preprocesses: A list of preprocesses to apply IN ORDER
-    """
+        model (Model): A Keras model for segmentation
+        num_classes (int): Number of classes
+        loss (str): A loss function used for training
+            -> available losses : ['cce', 'mse']
 
+        x_dir_path (str): Path to image dataset
+        y_dir_path (str): Path to label dataset
+
+        metrics (List[Metric]): A list of metrics to follow progress
+        preprocesses (List[Preprocess]): A list of preprocesses to apply IN ORDER
+        callbacks (List[Callback]): A list of callbacks 
+    """
     def __init__(
         self, 
         model: Model,
-        loss: str,
-        metrics: List['Metric'],
-        preprocesses: Optional[List['Preprocess']] = None
-     ) -> None:
-
-        self.model = model
-        self.loss = loss
-        self.metrics = metrics
-        self.preprocesses = preprocesses
-    
-    def train(
-        self, 
+        num_classes: int,
+        loss: str = 'cce',
         x_dir_path: str = "./dataset/rgb/",
         y_dir_path: str = "./dataset/seg/",
+        metrics: List['Metric'] = [],
+        preprocesses: Optional[List['Preprocess']] = [],
+        callbacks: Optional[List['Callback']] = []
+     ) -> None:
+
+        assert len(metrics) > 0, "Metrics must be specified"
+        for metric in metrics:
+            assert isinstance(metric, Metric), "Metrics must be of type Metric"
+
+        self.model = model
+
+        self.metrics = metrics
+        self.preprocesses = preprocesses
+        self.callbacks = callbacks
+
+        self.num_classes = num_classes
+
+        if loss == 'cce':
+            self.loss = CategoricalCrossentropy()
+        elif loss == 'mse':
+            self.loss = MeanSquaredError()
+        else:
+            warn(loss + " is not implemented, cce used by default")
+            self.loss = CategoricalCrossentropy()
+
+        self.x_dir_path = x_dir_path
+        self.y_dir_path = y_dir_path
+    
+
+    def train(
+        self, 
         num_epochs: int = 10,
-        batch_size: int = 12
+        batch_size: int = 12,
+        save_model_bool: bool = False,
     ) -> None:
-        """Trains the model
-        """
-
-        x_dir = sorted(os.listdir(x_dir_path))
-        y_dir = sorted(os.listdir(y_dir_path))
-
-        def load_batch(idx):
-
-            xs = []
-            ys = []
-            for i in idx:
-                ix = Image.open(x_dir_path+x_dir[i])
-                iy = Image.open(y_dir_path+y_dir[i])
-                xs.append(np.array(ix))
-                ys.append(np.array(iy))
-            x = tf.constant(np.array(xs), dtype=tf.float32)
-            y = tf.one_hot(tf.constant(np.array(ys)), num_classes, axis=-1, dtype=tf.float32)
-
-            return x, y
-
-        num_classes = 12
-        batch_size = batch_size
-        num_elem = len(os.listdir(x_dir_path))
-
-        pre = Crop(shape=(224,224,3))
-        mo = self.model
-        me = MeanIOU(num_classes)
+        """Trains the model"""
+        sorted_x_dir = sorted(os.listdir(self.x_dir_path))
+        sorted_y_dir = sorted(os.listdir(self.y_dir_path))
+        num_elem = len(sorted_x_dir)
 
         opt = tf.keras.optimizers.Adam(2e-4)
-        cce = tf.keras.losses.CategoricalCrossentropy()
-        # weights = tf.constant([0.1, 0.5, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1], dtype=tf.float32)
 
         idx = list(range(num_elem))
         random.shuffle(idx)
         self.train_idx = train_idx = idx[:int(len(idx)*0.8)]
         self.valid_idx = valid_idx = idx[int(len(idx)*0.8):int(len(idx)*0.9)]
         self.test_idx = test_idx = idx[int(len(idx)*0.9):]
-        for ep in tqdm(range(num_epochs)):
+
+        for epoch in tqdm(range(num_epochs)):
+
+            logs = {}
 
             # Train 
             random.shuffle(train_idx)
             num_batch = len(train_idx) // batch_size
-            for i in tqdm(range(num_batch), desc="inner loop"):
+            for i in tqdm(range(num_batch), desc="Inner training loop"):
                 idx_batch = train_idx[i*batch_size: (i+1)*batch_size]
-                xbatch, ybatch = load_batch(idx_batch)
-                xbatch = pre.func(xbatch)
-                ybatch = pre.func(ybatch)
+                train_xbatch, train_ybatch = load_batch(
+                    idx_batch,
+                    self.num_classes,
+                    self.x_dir_path,
+                    self.y_dir_path,
+                    sorted_x_dir,
+                    sorted_y_dir
+                )
+
+                for preprocess in self.preprocesses:
+                    train_xbatch, train_ybatch = preprocess.func(train_xbatch, train_ybatch)
+                train_ybatch = train_ybatch - tf.where(train_ybatch == 255., 255., 0.)
+
                 with tf.GradientTape() as tape:
-                    ypred = mo(xbatch)
-                    _y = tf.reshape(ybatch, shape=(-1, ybatch.shape[-1]))
-                    _ypred = tf.reshape(ypred, shape=(-1, ypred.shape[-1]))
-                    loss = cce(_y, _ypred)
-                    # loss = weighted_cce(_y, _ypred, weights)
-                grads = tape.gradient(loss, mo.trainable_weights)
-                opt.apply_gradients(zip(grads, mo.trainable_weights))
+                    train_ypred = self.model(train_xbatch)
+                    _y = tf.reshape(train_ybatch, shape=(-1, train_ybatch.shape[-1]))
+                    _ypred = tf.reshape(train_ypred, shape=(-1, train_ypred.shape[-1]))
+                    train_loss = self.loss(_y, _ypred)
+                    
+                grads = tape.gradient(train_loss, self.model.trainable_weights)
+                opt.apply_gradients(zip(grads, self.model.trainable_weights))
+
+            logs.update(train_loss=train_loss.numpy())
+            logs.update((metric.func(train_ybatch, train_ypred).numpy() for metric in self.metrics))
 
             # Valid
             random.shuffle(valid_idx)
             num_batch = len(valid_idx) // batch_size
             for i in range(num_batch):
-                idx_batch = train_idx[i*batch_size: (i+1)*batch_size]
-                vxbatch, vybatch = load_batch(idx_batch)
-                vxbatch = pre.func(vxbatch)
-                vybatch = pre.func(vybatch)
-                vypred = mo(vxbatch)
-                v_y = tf.reshape(vybatch, shape=(-1, vybatch.shape[-1]))
-                v_ypred = tf.reshape(vypred, shape=(-1, vypred.shape[-1]))
-                v_loss = cce(v_y, v_ypred)
+                idx_batch = valid_idx[i*batch_size: (i+1)*batch_size]
+                valid_xbatch, valid_ybatch = load_batch(
+                    idx_batch,
+                    self.num_classes,
+                    self.x_dir_path,
+                    self.y_dir_path,
+                    sorted_x_dir,
+                    sorted_y_dir
+                )
+                
+                for preprocess in self.preprocesses:
+                    valid_xbatch, valid_ybatch = preprocess.func(valid_xbatch, valid_ybatch)
+                valid_ybatch = valid_ybatch - tf.where(valid_ybatch == 255., 255., 0.)
+                
+                valid_ypred = self.model(valid_xbatch)
+                _y = tf.reshape(valid_ybatch, shape=(-1, valid_ybatch.shape[-1]))
+                _ypred = tf.reshape(valid_ypred, shape=(-1, valid_ypred.shape[-1]))
+                valid_loss = self.loss(_y, _ypred)
+            
+            logs.update(valid_loss=valid_loss.numpy())
+            logs.update((metric.func(valid_ybatch, valid_ypred).numpy() for metric in self.metrics))
 
-            self.model = mo
+            for callback in self.callbacks:
+                callback.at_epoch_end(logs=logs)
 
-            print("ep ", ep, " | loss ", loss.numpy(), " | iou ", me.func(ybatch, ypred).numpy(), " | v_loss ", v_loss.numpy(), " | v_iou ", me.func(vybatch, vypred).numpy())
+            if save_model_bool:
+                save_model(self.model, f"./results/model_at_{ep}")
+
+            print(f"Epoch {epoch+1}: ", logs)
+
+
+def load_batch(
+    idx: List[int],
+    num_classes: int,
+    x_dir_path: str,
+    y_dir_path: str,
+    sorted_x_dir: List[str],
+    sorted_y_dir: List[str],
+) -> Tuple[tf.Tensor, tf.Tensor]:
+    """Loads a batch of images from disk"""
+    xs = []
+    ys = []
+
+    for i in idx:
+        ix = Image.open(os.path.join(x_dir_path, sorted_x_dir[i]))
+        iy = Image.open(os.path.join(y_dir_path, sorted_y_dir[i]))
+        xs.append(np.array(ix))
+        ys.append(np.array(iy))
+
+    x = tf.constant(np.array(xs), dtype=tf.float32)
+    y = tf.one_hot(tf.constant(np.array(ys)), num_classes, axis=-1, dtype=tf.float32)
+
+    return x, y
