286c4df7923660b1474ce14a98ec36f70d85c11e
Raffaelbdl <raffaelbdl@gmail.com>
2022-05-24 10:49:47 +0200

    :sparkles: Add class mean iou

--- improved_nightdrive/pipeline/metric.py
+++ improved_nightdrive/pipeline/metric.py
@@ -1,4 +1,5 @@
 from functools import partial
+from typing import List
 
 import tensorflow as tf
 
@@ -27,6 +28,15 @@ class MeanIOU(Metric):
 
     def __init__(self, num_classes) -> None:
         super().__init__(name="miou", func=partial(mean_iou, num_classes=num_classes))
+
+
+class ClassMeanIOU(Metric):
+    """Mean IOU metric per class"""
+
+    def __init__(self, num_classes) -> None:
+        super().__init__(
+            name="cmiou", func=partial(class_mean_iou, num_classes=num_classes)
+        )
 
 
 def mean_iou(
@@ -59,3 +69,38 @@ def mean_iou(
     iou_score = tf.reduce_sum(intersection, (1, 2, 3)) / tf.reduce_sum(union, (1, 2, 3))
 
     return tf.reduce_mean(iou_score).numpy()
+
+
+def class_mean_iou(
+    labels: tf.Tensor,
+    predictions: tf.Tensor,
+    num_classes: int,
+) -> List[float]:
+    """Computes the mean IOU for a batch for each class
+
+    Args:
+        labels (tf.Tensor): The segmentation map label,
+            as tensor of shape [batch_size, height, width, num_classes]
+            or as tensor of shape [batch_size, height, width] (will be one-hotted)
+        predictions (tf.Tensor): The segmentation map prediction,
+            as softmax [batch_size, height, width, num_classes]
+        num_classes (int): Number of classes
+
+    Returns:
+        mean_iou_score (List[float]): The Mean IoU Score for each class
+    """
+    if labels.shape[-1] != num_classes:
+        labels = tf.one_hot(labels, num_classes)
+
+    _labels = tf.cast(labels, dtype=tf.int32)
+    _predictions = tf.one_hot(
+        tf.argmax(predictions, axis=-1), num_classes, dtype=tf.int32
+    )
+    intersection = _labels * _predictions
+    union = _labels + _predictions - intersection
+    iou_score = tf.math.divide_no_nan(
+        tf.cast(tf.reduce_sum(intersection, (1, 2)), tf.float32),
+        tf.cast(tf.reduce_sum(union, (1, 2)), tf.float32),
+    )
+
+    return tf.reduce_mean(iou_score, axis=0).numpy()
