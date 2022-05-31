02357407c1f85e31c8bb897b7a3150299c565fcb
Raffaelbdl <raffaelbdl@gmail.com>
2022-05-31 15:07:35 +0200

    :fire: Delete apply lighting

--- improved_nightdrive/pipeline/lighting.py
+++ improved_nightdrive/pipeline/lighting.py
@@ -1,36 +1,14 @@
 """Scripts to apply local image process"""
-from typing import List, Optional
+from typing import Optional
 
 import matplotlib.pyplot as plt
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras import Model
-from tensorflow_addons.image import gaussian_filter2d
-
-from improved_nightdrive.pipeline.preprocess import GammaProcess
-
-
-def apply_lighting(
-    original_input: np.ndarray,
-    prediction: np.ndarray,
-    gamma_list: List[float],
-) -> np.ndarray:
-
-    gamma_process = GammaProcess()
-    gamma_lists = [[g, g, g] for g in gamma_list]
-    modified_inputs = gamma_process.masked_func(
-        original_input,
-        gamma_lists,
-        prediction,
-    )
-
-    return modified_inputs
 
 
 def plot_mask(
     prediction: tf.Tensor, mask_indice: int, original: Optional[tf.Tensor] = None
 ) -> None:
-
+    """Plot a prediction mask"""
     assert len(prediction.shape) == 3
     masked_input = original * prediction[..., mask_indice, None]
     if original is not None:
@@ -50,13 +28,8 @@ def plot_mask(
 
 
 def apply_threshold(prediction: tf.Tensor, threshold: float) -> tf.Tensor:
+    """Apply a threshold on image"""
     return tf.where(prediction > threshold, 1.0, 0.0)
-
-
-def apply_gaussian_blur(
-    prediction: tf.Tensor, kernel: int, sigma: float = 1.0
-) -> tf.Tensor:
-    return gaussian_filter2d(prediction, kernel, sigma)
 
 
 def apply_gamma_map(
@@ -66,6 +39,7 @@ def apply_gamma_map(
     max_gamma: float = 0.5,
     min_gamma: float = 0.1,
 ):
+    """Apply gamma process to all pixels"""
     gamma_mask = mask * (min_gamma - max_gamma) + max_gamma
     gamma_mask = gamma_mask[..., mask_indice, None]
     input = input**gamma_mask
