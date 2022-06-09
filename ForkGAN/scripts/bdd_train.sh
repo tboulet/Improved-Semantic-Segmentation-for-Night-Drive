7bc3e5a3ba550e01fa0c18102e484d903b8fb278
Raffaelbdl <raffaelbdl@gmail.com>
2022-06-09 20:17:57 +0200

    :fire: Remove ForkGAN

--- ForkGAN/scripts/bdd_train.sh
+++
@@ -1,2 +0,0 @@
-### train
-python main.py --phase train --dataset_dir BDD100K --epoch 10 --gpu 0 --n_d 2 --n_scale 2 --checkpoint_dir ./check/BDD100K --sample_dir ./check/BDD100K/sample --continue_train True --L1_lambda 10 --load_size 572 --fine_size 512
